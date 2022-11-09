9e4b37f86c0131da3221a7c41831bc9cfaa47a9e
qkrwnstlr <akfldkskgorn@gmail.com>
2022-11-09 15:10:40 +0900

    2022_11_09 update

--- app/src/main/java/kr/ac/kumoh/s20180489/w1001intentdata/ImageActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20180489/w1001intentdata/ImageActivity.kt
@@ -1,20 +1,54 @@
 package kr.ac.kumoh.s20180489.w1001intentdata
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import kr.ac.kumoh.s20180489.w1001intentdata.databinding.ActivityImageBinding
 
-class ImageActivity : AppCompatActivity() {
-    val binding by lazy { ActivityImageBinding.inflate(layoutInflater) }
+class ImageActivity : AppCompatActivity(),
+    View.OnClickListener {
+
+    companion object {
+        const val imageName = "image"
+        const val resultName = "result"
+
+        const val LIKE = 10
+        const val DISLIKE = 20
+        const val NONE = 0
+    }
+
+    private lateinit var binding: ActivityImageBinding
+    private var image: String? = null
+
+    override fun onClick(v: View?) {
+        val intent = Intent()
+        val value = when (v?.id) {
+            binding.btnLike.id -> LIKE
+            binding.btnDislike.id -> DISLIKE
+            else -> NONE
+        }
+        intent.putExtra(imageName, image)
+        intent.putExtra(resultName, value)
+        setResult(RESULT_OK, intent)
+        finish()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityImageBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val res = when (intent.getStringExtra(MainActivity.keyName)) {
+
+        image = intent.getStringExtra(MainActivity.keyName)
+        val res = when (image) {
             "gundam" -> R.drawable.gundam
             "zaku" -> R.drawable.zaku
             else -> R.drawable.ic_launcher_foreground
         }
         binding.imgGundam.setImageResource(res)
+
+        binding.btnLike.setOnClickListener(this)
+        binding.btnDislike.setOnClickListener(this)
     }
 }
--- app/src/main/java/kr/ac/kumoh/s20180489/w1001intentdata/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20180489/w1001intentdata/MainActivity.kt
@@ -4,12 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import kr.ac.kumoh.s20180489.w1001intentdata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     companion object {
         const val keyName = "image"
     }
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var launcher: ActivityResultLauncher<Intent>
 
     override fun onClick(v: View?) {
         val intent = Intent(this, ImageActivity::class.java)
@@ -19,10 +24,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             else -> return
         }
         intent.putExtra(keyName, value)
-        startActivity(intent)
+        launcher.launch(intent)
     }
-
-    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +33,25 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         with(binding){
             btnZaku.setOnClickListener(this@MainActivity)
             btnGundam.setOnClickListener(this@MainActivity)
+        }
+        launcher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()) {
+
+            if (it.resultCode != RESULT_OK)
+                return@registerForActivityResult
+
+            val result = it.data?.getIntExtra(ImageActivity.resultName,
+                ImageActivity.NONE)
+            val str = when (result) {
+                ImageActivity.LIKE -> "좋아요"
+                ImageActivity.DISLIKE -> "싫어요"
+                else -> ""
+            }
+            val image = it.data?.getStringExtra(ImageActivity.imageName)
+            when (image) {
+                "gundam" -> binding.btnGundam.text = "건담 ($str)"
+                "zaku" -> binding.btnZaku.text = "자쿠 ($str)"
+            }
         }
     }
 }
